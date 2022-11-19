d7372fbfeb6849ada405c39795e08456dca14895
rwxayheee <ayhe1768@gmail.com>
2022-11-19 15:34:07 -0500

    fix iterate full list

--- io.py
+++ io.py
@@ -66,7 +66,7 @@ def main():
                 header = ["x", "y", "z"]
         csvwriter.writerow(header)
 
-        for pose in pose_list[:3]:
+        for pose in pose_list:
             pose_propvals = []
             for val in pose.propertydic.values():
                 if type(val)==list and len(val)==1:
